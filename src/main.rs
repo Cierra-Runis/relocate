f0b04f241ccf8e87f6b6064b851a18201e916978
Cierra-Runis <byrdsaron@gmail.com>
2026-02-03 20:23:20 +0900

    refactor: update MIDI file handling and error management

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 mod midi;
 
-use crate::midi::MIDIFile;
+use crate::midi::{Chunk, MIDIFile};
 use std::fs;
 
 fn main() {
     let path = "./assets/Lapis Lazuli.mid";
     let bytes = fs::read(path).expect("Failed to read MIDI file");
-    let midi_file = MIDIFile::new(bytes);
-    match midi_file.chunks() {
+    let midi_file = MIDIFile::from(bytes);
+
+    match Vec::<Chunk>::try_from(midi_file) {
         Ok(chunks) => {
             for chunk in chunks {
-                println!("{:?}", chunk);
+                println!("{chunk}");
             }
         }
-        Err(e) => {
-            eprintln!("Error parsing MIDI file: {}", e);
-        }
+        Err(e) => eprintln!("Error parsing MIDI file: {}", e),
     }
 }
--- src/midi.rs
+++ src/midi.rs
@@ -1,54 +1,60 @@
 use derive_more::{Debug, Display, Error};
-use pretty_hex::PrettyHex;
 
-#[derive(Debug, derive_more::Display, Error)]
-pub enum MIDIError {
-    #[display("Unexpected End of File")]
-    UnexpectedEndOfFile,
-    #[display("Invalid Chunk Header")]
-    InvalidChunkHeader,
-    #[display("Invalid Chunk Length")]
-    InvalidChunkLength,
-}
-
-/// To any file system, a [MIDI File](MIDIFile) is simply a [series of 8-bit bytes](Vec<u8>).
+/// To any file system, a [MIDI File](MIDIFile) is simply a [series of 8-bit
+/// bytes](Vec<u8>).
 #[derive(Debug, Display, Clone)]
 #[display("{}", pretty_hex::pretty_hex(_0))]
 pub struct MIDIFile(Vec<u8>);
 
-impl MIDIFile {
-    pub fn new(bytes: Vec<u8>) -> Self {
+impl From<Vec<u8>> for MIDIFile {
+    fn from(bytes: Vec<u8>) -> Self {
         MIDIFile(bytes)
     }
+}
+
+#[derive(Debug, derive_more::Display, Error)]
+pub enum TryFromMIDIFileError {
+    #[debug("Incomplete chunk prefix: file ended before reading 8-byte prefix")]
+    IncompleteChunkPrefix,
+    #[debug("Malformed chunk type: failed to parse 4-byte type identifier")]
+    MalformedChunkType,
+    #[debug("Malformed chunk length: failed to parse 4-byte length field")]
+    MalformedChunkLength,
+    #[debug("Truncated chunk data: declared length exceeds remaining file size")]
+    TruncatedChunkData,
+}
+
+impl TryFrom<MIDIFile> for Vec<Chunk> {
+    type Error = TryFromMIDIFileError;
 
     /// [MIDI File](MIDIFile)s are made up of [chunk](Chunk)s.
-    pub fn chunks(&self) -> Result<Vec<Chunk>, MIDIError> {
+    fn try_from(midi_file: MIDIFile) -> Result<Self, Self::Error> {
         let mut chunks = Vec::new();
         let mut i = 0;
 
-        while i < self.0.len() {
-            if i + 8 > self.0.len() {
-                return Err(MIDIError::UnexpectedEndOfFile);
+        while i < midi_file.0.len() {
+            if i + 8 > midi_file.0.len() {
+                return Err(TryFromMIDIFileError::IncompleteChunkPrefix);
             }
 
-            let kind_bytes: [u8; 4] = self.0[i..i + 4]
+            let kind_bytes: [u8; 4] = midi_file.0[i..i + 4]
                 .try_into()
-                .map_err(|_| MIDIError::InvalidChunkHeader)?;
+                .map_err(|_| TryFromMIDIFileError::MalformedChunkType)?;
             let kind = ChunkKind::from(kind_bytes);
 
-            let length_bytes: [u8; 4] = self.0[i + 4..i + 8]
+            let length_bytes: [u8; 4] = midi_file.0[i + 4..i + 8]
                 .try_into()
-                .map_err(|_| MIDIError::InvalidChunkHeader)?;
+                .map_err(|_| TryFromMIDIFileError::MalformedChunkLength)?;
             let length = u32::from_be_bytes(length_bytes);
 
             let data_start = i + 8;
             let data_end = data_start + length as usize;
 
-            if data_end > self.0.len() {
-                return Err(MIDIError::InvalidChunkLength);
+            if data_end > midi_file.0.len() {
+                return Err(TryFromMIDIFileError::TruncatedChunkData);
             }
 
-            let data = self.0[data_start..data_end].to_vec();
+            let data = midi_file.0[data_start..data_end].to_vec();
 
             chunks.push(Chunk { kind, length, data });
 
@@ -59,11 +65,17 @@ impl MIDIFile {
     }
 }
 
-/// Each [chunk](Chunk) has a 4-character [type](Chunk::kind) and a 32-bit
-/// [length](Chunk::length), which is the number of bytes in the [chunk](Chunk).
+/// Each [chunk] has a 4-character [type] and a 32-bit [length], which is the
+/// number of bytes in the [chunk].
 ///
-/// This structure allows future [chunk type](ChunkKind)s to be designed which
-/// may easily be ignored if encountered by a program written before the [chunk type](ChunkKind) is introduced.
+/// This structure allows future [chunk type]s to be designed which may easily
+/// be ignored if encountered by a program written before the [chunk type] is
+/// introduced.
+///
+/// [chunk]: Chunk
+/// [type]: Chunk::kind
+/// [length]: Chunk::length
+/// [chunk type]: ChunkKind
 #[derive(Debug, Display)]
 #[debug("Chunk: kind={:?}, length={:?}, data={}", kind, length, pretty_hex::pretty_hex(&data))]
 #[display("Chunk: kind={}, length={}", kind, length)]
@@ -72,37 +84,40 @@ pub struct Chunk {
     pub kind: ChunkKind,
 
     /// The 4-character ASCII [type](Chunk::kind) is followed by a 32-bit
-    /// [length](Chunk::length), most significant byte first (a length of 6 is stored as `00 00 00 06`).
+    /// [length](Chunk::length), most significant byte first (a length of 6 is
+    /// stored as `00 00 00 06`).
     ///
-    /// This [length](Chunk::length) refers to the number of bytes of [data](Chunk::data) which follow:
-    /// the eight bytes of [type](Chunk::kind) and [length](Chunk::length) are not included.
+    /// This [length](Chunk::length) refers to the number of bytes of
+    /// [data](Chunk::data) which follow: the eight bytes of [type](Chunk::kind)
+    /// and [length](Chunk::length) are not included.
     ///
-    /// Therefore, a chunk with a [length](Chunk::length) of 6 would actually occupy 14 bytes in the disk file.
+    /// Therefore, a chunk with a [length](Chunk::length) of 6 would actually
+    /// occupy 14 bytes in the disk file.
     pub length: u32,
 
     pub data: Vec<u8>,
 }
 
 /// [MIDI File](MIDIFile)s contain two [types of chunk](ChunkKind)s:
-/// [header chunk](ChunkType::Header)s and [track chunk](ChunkType::Track)s.
+/// [header chunk](ChunkKind::Header)s and [track chunk](ChunkKind::Track)s.
 ///
-/// The concepts of multiple tracks, multiple MIDI outputs, patterns, sequences, and
-/// songs may all be implemented using several track [chunk](Chunk)s.
+/// The concepts of multiple tracks, multiple MIDI outputs, patterns, sequences,
+/// and songs may all be implemented using several track [chunk](Chunk)s.
 #[derive(Debug, Display)]
 pub enum ChunkKind {
-    /// A [header chunk](ChunkType::Header) provides a minimal amount
+    /// A [header chunk](ChunkKind::Header) provides a minimal amount
     /// of information pertaining to the entire [`MIDIFile`].
     #[debug("Header: {:?}", _0)]
     #[display("Header")]
     Header([u8; 4]),
 
-    /// A [track chunk](ChunkType::Track) contains a sequential stream
+    /// A [track chunk](ChunkKind::Track) contains a sequential stream
     /// of MIDI data which may contain information for up to 16 MIDI channels.
     #[debug("Track: {:?}", _0)]
     #[display("Track")]
     Track([u8; 4]),
 
-    /// Your programs should _expect_ [alien chunk](ChunkType::Alien)s
+    /// Your programs should _expect_ [alien chunk](ChunkKind::Alien)s
     /// and treat them as if they weren't there.
     #[debug("Alien: {:?}", _0)]
     #[display("Alien")]
