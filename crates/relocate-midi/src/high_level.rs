990d654083608b7bee9ab9a3d8966580669d5e89
Cierra-Runis <byrdsaron@gmail.com>
2026-02-06 00:30:15 +0900

    refactor: remove high_level module from MIDI crate

--- crates/relocate-midi/src/high_level.rs
+++
@@ -1,6 +0,0 @@
-use derive_more::{Debug, Deref};
-
-use crate::file::midi::MIDIFile;
-
-#[derive(Debug, Deref)]
-pub struct MIDI(MIDIFile);
--- crates/relocate-midi/src/lib.rs
+++ crates/relocate-midi/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod core;
 pub mod event;
 pub mod file;
-pub mod high_level;
 mod scanner;
