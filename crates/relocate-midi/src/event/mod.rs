bbe8bbfccfdfab23251f6fa26b60a7809b71aba0
Cierra-Runis <byrdsaron@gmail.com>
2026-02-05 14:42:59 +0900

    feat: add MIDIEvent and SystemExclusiveEvent enums to event module

--- crates/relocate-midi/src/event/mod.rs
+++ crates/relocate-midi/src/event/mod.rs
@@ -1,3 +1,9 @@
 pub mod meta;
 pub mod midi;
 pub mod system_exclusive;
+
+pub enum Event {
+    Meta(meta::MetaEvent),
+    MIDI(midi::MIDIEvent),
+    SystemExclusive(system_exclusive::SystemExclusiveEvent),
+}
