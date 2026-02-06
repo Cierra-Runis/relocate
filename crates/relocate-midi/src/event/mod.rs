a2cbb867b0be7c741cf75eaba93342d7d78fdabf
Cierra-Runis <byrdsaron@gmail.com>
2026-02-06 15:54:15 +0900

    refactor: remove unused MIDIEvent and SystemExclusiveEvent enums

--- crates/relocate-midi/src/event/mod.rs
+++ crates/relocate-midi/src/event/mod.rs
@@ -1,9 +1,2 @@
+pub mod file;
 pub mod meta;
-pub mod midi;
-pub mod system_exclusive;
-
-pub enum Event {
-    Meta(meta::MetaEvent),
-    MIDI(midi::MIDIEvent),
-    SystemExclusive(system_exclusive::SystemExclusiveEvent),
-}
